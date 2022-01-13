61666ac4498d2bea709767adae5a8915f7106cef
rosangela <roliveirasv@gmail.com>
2022-01-12 22:26:42 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+function Navbar(){
+    return (
+        <header>
+        <nav className="container">
+          <div className= "dsmovie-nav-content">
+            <h1>DsMovie</h1>
+            <a href="https://github.com/rosangelasilva1" > 
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/rosangelasilva1</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+}
+export default Navbar
